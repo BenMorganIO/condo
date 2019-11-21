a82e21991f1af7c76d95bf13f2d14d718f0d8474
Ben A. Morgan <ben@benmorgan.io>
2019-11-21 18:56:19 -0500

    fix some issues

--- lib/mix/tasks/condo.migrate.ex
+++ lib/mix/tasks/condo.migrate.ex
@@ -2,22 +2,21 @@ defmodule Mix.Tasks.Condo.Migrate do
   use Mix.Task
 
   import Mix.Ecto
-  import Mix.EctoSQL
 
   @shortdoc "Runs the migrations for the tenants."
 
   def run(args) do
     no_umbrella!("condo.migrate")
-    repos = parse_repo(args)
+    repos = Mix.Ecto.parse_repo(args)
 
-    Enum.each repos, fn repo ->
-      ensure_repo(repo, args)
-      ensure_started(repo, [])
+    Enum.each(repos, fn repo ->
+      Ecto.Migrator.with_repo(repo, fn repo ->
+        stream = repo.stream(repo.tenant_ids)
 
-      stream = repo.stream(repo.tenant_ids)
-      repo.transaction fn ->
-        Enum.each(stream, &Condo.migrate_tenant(repo, &1))
-      end
-    end
+        repo.transaction(fn ->
+          Enum.each(stream, &Condo.migrate_tenant(repo, &1))
+        end)
+      end)
+    end)
   end
 end
--- lib/mix/tasks/condo.rollback.ex
+++ lib/mix/tasks/condo.rollback.ex
@@ -2,7 +2,6 @@ defmodule Mix.Tasks.Condo.Rollback do
   use Mix.Task
 
   import Mix.Ecto
-  import Mix.EctoSQL
 
   @shortdoc "Rollsback the latest migration for each tenant."
 
@@ -10,14 +9,14 @@ defmodule Mix.Tasks.Condo.Rollback do
     no_umbrella!("condo.rollback")
     repos = parse_repo(args)
 
-    Enum.each repos, fn repo ->
-      ensure_repo(repo, args)
-      ensure_started(repo, [])
+    Enum.each(repos, fn repo ->
+      Ecto.Migrator.with_repo(repo, fn repo ->
+        stream = repo.stream(repo.tenant_ids)
 
-      stream = repo.stream(repo.tenant_ids)
-      repo.transaction fn ->
-        Enum.each(stream, &Condo.rollback_tenant(repo, &1))
-      end
-    end
+        repo.transaction(fn ->
+          Enum.each(stream, &Condo.rollback_tenant(repo, &1))
+        end)
+      end)
+    end)
   end
 end
